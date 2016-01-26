eb9726ba445f5bc9d7ff873302bcf80acbad6fee
Chris Kuiper and rob reasoner <chris.kuiper@gmail.com>
2016-01-26 10:23:51 -0800

    add address html with append for scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -19,12 +19,30 @@ Address.prototype.fullAddress = function() {
 }
 
 $(document).ready(function() {
+  $("#add-address").click(function() {
+    $("new-addresses").append('<div class ="new-addresses">' +
+                                '<div class="form-group">' +
+                                  '<label for="new-street">Street</label>' +
+                                  '<input type="text" class="form-control new-street">' +
+                                '</div>' +
+                                '<div class="form-group">' +
+                                  '<label for="new-city">City</label>' +
+                                  '<input type="text" class="form-control new-city">' +
+                                '</div>' +
+                                '<div class="form-group">' +
+                                  '<label for="new-state">State</label>' +
+                                  '<input type="text" class="form-control new-state">' +
+                                '</div>' +
+                              '</div>');
+  });
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
